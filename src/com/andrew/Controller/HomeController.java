9a429abe03e0811ea8cfb49d1a15a22f45b262a5
AndrewDi <pandipd@outlook.com>
2017-07-30 16:09:28 +0800

    Add Bootstrap CSS

--- src/com/andrew/Controller/HomeController.java
+++ src/com/andrew/Controller/HomeController.java
@@ -1,15 +1,17 @@
 package com.andrew.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/Home")
-public class TestController {
+public class HomeController {
 
     @RequestMapping("hello")
-    public String hello(){
-
+    public String hello(ModelMap modelMap){
+        modelMap.addAttribute("name","hello,world");
         return "Home/Index";
     }
 }
