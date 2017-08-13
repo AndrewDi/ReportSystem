cf6714f61415e2dcffcead726db57cb6a5bfa782
AndrewDi <pandipd@outlook.com>
2017-08-13 11:34:57 +0800

    Add Aviator Support

--- src/com/andrew/Common/ArrayUtils.java
+++ src/com/andrew/Common/ArrayUtils.java
@@ -1,14 +1,10 @@
 package com.andrew.Common;
 
 import com.alibaba.druid.util.StringUtils;
-import com.google.common.base.Splitter;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class ArrayUtils {
 
@@ -18,7 +14,7 @@ public class ArrayUtils {
      */
     public static List<String[]> strToList(String str){
         List<String[]> arrays=new ArrayList<>();
-        List<String> list = Splitter.on("\n").trimResults().splitToList(str);
+        List<String> list = Arrays.asList(str.split("\n"));
         for (String data:list) {
             if(StringUtils.isEmpty(data))
                 continue;
